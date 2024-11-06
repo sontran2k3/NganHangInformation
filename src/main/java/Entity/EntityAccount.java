a47f3822e57c9e16397d1262d5caf134998b8514
Trần Khắc Sơn <120206879+sontran2k3@users.noreply.github.com>
2024-11-06 15:11:12 +0700

    06/11/2024

---
+++ src/main/java/Entity/EntityAccount.java
@@ -0,0 +1,71 @@
+package Entity;
+
+import java.math.BigDecimal;
+
+public class EntityAccount {
+    private int customerId;
+    private String fullname;
+    private String accountType;
+    private BigDecimal balance;
+    private String address;
+    private String status;
+
+    public int getPin() {
+        return pin;
+    }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+
+    private int pin;
+
+    // Getters and Setters
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
